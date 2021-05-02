63fb918d0c1e394eccb2c6cf5f1f5ab13da5f6e9
Aadarshkashyap51 <aadarshkashyap51@gmail.com>
2021-05-02 19:14:51 +0530

    imgslider started

---
+++ src/components/ImgSlider.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Slider from "react-slick";
+
+function ImgSlider() {
+    return (
+        <div>
+            Image Slider
+        </div>
+    )
+}
+
+export default ImgSlider
