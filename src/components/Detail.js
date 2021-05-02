355baff10a7adac4f7065e9a711fbe881e000c7e
Aadarshkashyap51 <aadarshkashyap51@gmail.com>
2021-05-02 22:24:47 +0530

    Router Added

---
+++ src/components/Detail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Detail() {
+    return (
+        <div>
+            Detail
+        </div>
+    )
+}
+
+export default Detail
