b00855ca4d655efa5cb597e845392d74832dec2f
Aadarshkashyap51 <aadarshkashyap51@gmail.com>
2021-05-02 21:20:16 +0530

    Viewers Done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
 import ImgSlider from './ImgSlider'
+import Viewers from './Viewers'
 function Home() {
     return (
         <Container>
             <ImgSlider />
+            <Viewers />
         </Container>
     )
 }
