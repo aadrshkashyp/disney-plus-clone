2107fed07fd8b1e470f701fd27012f941cf51f32
Aadarshkashyap51 <aadarshkashyap51@gmail.com>
2021-05-02 20:17:28 +0530

    ImgSlider overflow issue stage

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -1,12 +1,52 @@
 import React from 'react'
+import styled from 'styled-components'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-
 function ImgSlider() {
+
+    let settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        auroplay: true
+    }
+
     return (
-        <div>
-            Image Slider
-        </div>
+        <Carousel {...settings}>
+            <Wrap>
+                <img src="/images/slider-badging.jpg" alt="" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/slider-badag.jpg" alt="" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/slider-badag.jpg" alt="" />
+            </Wrap>
+        </Carousel>
     )
 }
 
 export default ImgSlider
+
+const Carousel = styled(Slider)
+    `
+         margin-top: 20px:
+
+         .slick-list {
+             overflow: visible;
+         }
+    `
+
+const Wrap = styled.div
+    `
+        img {
+            border-radius: 4px;
+            width: 100%;
+            height: 100%;
+            box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+
+        }
+    `
